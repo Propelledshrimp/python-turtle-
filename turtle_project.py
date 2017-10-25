59976b6bde5e1e387e0aa4b4f7974a03a1d18057
Tatem Pearson <jpears1974@greenvilleschools.us>
2017-10-25 09:29:20 -0400

    made a player turtle and fixed the backround

--- turtle_project.py
+++ turtle_project.py
@@ -2,18 +2,22 @@ from turtle import *
 import math
 import random  
 
+god = Turtle() 
 tatem = Turtle()
 veronica = Turtle()
 hailey = Turtle()
+tatem.shape("turtle")
+veronica.shape("turtle")
+hailey.shape("turtle") 
 
 all_turtles = [ tatem, veronica, hailey]
 
 green_hill = Screen()
-green_hill.screensize(600,219)
+green_hill.screensize(2000,2000)
 green_hill.title("Turtle Plains")
 
 green_hill.bgpic() 
-green_hill.bgpic("turtlebackground.png")
+green_hill.bgpic("turtle_background.png")
 
 
 def draw_star(t, x, y , points, line, fill):
@@ -30,10 +34,31 @@ def draw_star(t, x, y , points, line, fill):
         t.forward(200)
         t.left(turn)
     t.end_fill()
+    t.hideturtle() 
+def up_arrow():
+    tatem.penup()
+    tatem.fd(100)
+def down_arrow():
+    tatem.penup() 
+    tatem.back(100)
+def right_arrow():
+    tatem.penup() 
+    tatem.right(45)
+def left_arrow():
+    tatem.penup() 
+    tatem.left(45)  
+    
+onkeypress(up_arrow, "Up")
+onkeypress(down_arrow, "Down")
+onkeypress(left_arrow, "Left")
+onkeypress(right_arrow, "Right")
+listen() 
+    
 
 
 
         
 
-draw_star(tatem, 200, 200, 36 , 'orange' , 'yellow')
+draw_star(god, 200, 200, 36 , 'orange' , 'yellow')
+
  
